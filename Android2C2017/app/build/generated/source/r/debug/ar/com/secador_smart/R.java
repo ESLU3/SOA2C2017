408ac6ee785284115944ee591f34b9a2521c026c
ESLU3 <e.luciano@live.com.ar>
2017-11-29 08:36:59 -0300

    Android

--- Android2C2017/app/build/generated/source/r/debug/ar/com/secador_smart/R.java
+++ Android2C2017/app/build/generated/source/r/debug/ar/com/secador_smart/R.java
@@ -49,7 +49,15 @@ public final class R {
         public static final int text_view_paired=0x7f050007;
     }
     public static final class style {
+        /** 
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+         */
         public static final int AppBaseTheme=0x7f060000;
+        /**  All customizations that are NOT specific to a particular API-level can go here. 
+         */
         public static final int AppTheme=0x7f060001;
     }
 }
